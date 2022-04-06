0aff6e1bd09ab3f494161155b94725b35768bcb4
maggieeagle <oryol.margo@gmail.com>
2022-04-06 16:37:19 +0300

    Print combinations of three digits

---
+++ printcomb.go
@@ -0,0 +1,23 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func printComb() {
+	digits := []rune("0123456789")
+	for i := 0; i < len(digits)-2; i++ {
+		for j := i + 1; j < len(digits)-1; j++ {
+			for k := j + 1; k < len(digits); k++ {
+				z01.PrintRune(digits[i])
+				z01.PrintRune(digits[j])
+				z01.PrintRune(digits[k])
+				if !(i == 7 && j == 8 && k == 9) {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}

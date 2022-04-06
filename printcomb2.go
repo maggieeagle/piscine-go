0e163930b656c59fb581b04db3639a6b5b0f6823
maggieeagle <oryol.margo@gmail.com>
2022-04-06 16:54:06 +0300

    All combination of two different two-digit numbers

---
+++ printcomb2.go
@@ -0,0 +1,30 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintComb2() {
+	digits := []rune("0123456789")
+
+	for i := 1; i < len(digits); i++ {
+		for j := 0; j < len(digits); j++ {
+			for k := 1; k < len(digits); k++ {
+				for l := 0; l < len(digits); l++ {
+					if !(i == k && j == l) {
+						z01.PrintRune(digits[i])
+						z01.PrintRune(digits[j])
+						z01.PrintRune(digits[k])
+						z01.PrintRune(digits[l])
+
+						if !(i == 9 && j == 9 && k == 9 && l == 8) {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}

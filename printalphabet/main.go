ac80e9851bb484fd132c7c7dbc253f9beaf06a6b
maggieeagle <oryol.margo@gmail.com>
2022-04-06 14:35:36 +0300

    Print alphabet with a nice formatting

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -5,9 +5,11 @@ import (
 )
 
 func main() {
-	alphabet := []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	alphabet := []rune{
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-		't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'}
+		't', 'u', 'v', 'w', 'x', 'y', 'z', '\n',
+	}
 	for i := 0; i < 27; i++ {
 		z01.PrintRune(alphabet[i])
 	}

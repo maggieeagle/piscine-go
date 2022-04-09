a072dc738350a755b74ff1918a2c0c646fced79f
maggieeagle <oryol.margo@gmail.com>
2022-04-09 10:50:34 +0300

    All quads in one program

---
+++ quad.go
@@ -0,0 +1,98 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintQuad(x, y int, s1, s2, s3, s4, s5, s6 rune) {
+	if y > 0 {
+		if x > 0 {
+			z01.PrintRune(s1)
+		}
+		for i := 1; i < x-1; i++ {
+			z01.PrintRune(s2)
+		}
+		if x > 1 {
+			z01.PrintRune(s3)
+		}
+		println()
+	}
+	for j := 1; j < y-1; j++ {
+		if x > 0 {
+			z01.PrintRune(s6)
+		}
+		for i := 1; i < x-1; i++ {
+			z01.PrintRune(' ')
+		}
+		if x > 1 {
+			z01.PrintRune(s6)
+		}
+		println()
+	}
+	if y > 1 {
+		if x > 0 {
+			z01.PrintRune(s4)
+		}
+		for i := 1; i < x-1; i++ {
+			z01.PrintRune(s2)
+		}
+		if x > 1 {
+			z01.PrintRune(s5)
+		}
+		println()
+	}
+}
+
+//s1-s2-s3
+//s6	s6
+//s4-s2-s5
+
+func QuadA(x, y int) {
+	s1 := 'o'
+	s2 := '-'
+	s3 := 'o'
+	s4 := 'o'
+	s5 := 'o'
+	s6 := '|'
+	PrintQuad(x, y, s1, s2, s3, s4, s5, s6)
+}
+
+func QuadB(x, y int) {
+	s1 := '/'
+	s2 := '*'
+	s3 := '\\'
+	s4 := '\\'
+	s5 := '/'
+	s6 := '*'
+	PrintQuad(x, y, s1, s2, s3, s4, s5, s6)
+}
+
+func QuadC(x, y int) {
+	s1 := 'A'
+	s2 := 'B'
+	s3 := 'A'
+	s4 := 'C'
+	s5 := 'C'
+	s6 := 'B'
+	PrintQuad(x, y, s1, s2, s3, s4, s5, s6)
+}
+
+func QuadD(x, y int) {
+	s1 := 'A'
+	s2 := 'B'
+	s3 := 'C'
+	s4 := 'A'
+	s5 := 'C'
+	s6 := 'B'
+	PrintQuad(x, y, s1, s2, s3, s4, s5, s6)
+}
+
+func QuadE(x, y int) {
+	s1 := 'A'
+	s2 := 'B'
+	s3 := 'C'
+	s4 := 'C'
+	s5 := 'A'
+	s6 := 'B'
+	PrintQuad(x, y, s1, s2, s3, s4, s5, s6)
+}

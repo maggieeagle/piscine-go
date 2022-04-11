e844057577ae39f3914de849f8820da68030d320
maggieeagle <oryol.margo@gmail.com>
2022-04-11 14:08:14 +0300

    Recursive factorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,19 +1,21 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
+
 	if nb < 0 {
 		return 0
 	}
 
-	if nb <= 1 {
+	if nb == 0 || nb == 1 {
 		return 1
 	}
 
-	res := nb * RecursiveFactorial(nb-1)
+	prevRes := RecursiveFactorial(nb - 1)
+	println(prevRes)
 
-	if res < 0 {
-		return 0
-	} else {
-		return res
+	if nb*prevRes > prevRes {
+		return nb * prevRes
 	}
+
+	return 0
 }

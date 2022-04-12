35f657a8939cd9d3ba4bf24e4d7dcede970adc7a
maggieeagle <oryol.margo@gmail.com>
2022-04-12 12:15:24 +0300

    Compare strings

--- compare.go
+++ compare.go
@@ -1,23 +1,11 @@
 package piscine
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
 func Compare(a, b string) int {
-	str1 := []rune(a)
-	str2 := []rune(b)
-
-	for i := 0; i < min(len(str1), len(str2)); i++ {
-		if str1[i] < str2[i] {
-			return -1
-		}
+	if a == b {
+		return 0
 	}
-	if len(str1) > len(str2) {
-		return 1
+	if a < b {
+		return -1
 	}
-	return 0
+	return 1
 }

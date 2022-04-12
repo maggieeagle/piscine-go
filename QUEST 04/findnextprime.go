bd91d9c32fc3ba7d4da346eebed98e0abb32772f
maggieeagle <oryol.margo@gmail.com>
2022-04-12 10:02:30 +0300

    Moved to QUEST 04

---
+++ QUEST 04/findnextprime.go
@@ -0,0 +1,13 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb <= 1 {
+		return 2
+	}
+	for i := nb; i < nb*2; i++ {
+		if IsPrime(i) {
+			return i
+		}
+	}
+	return 0
+}
---
+++ QUEST 04/iterativefactorial.go
@@ -0,0 +1,18 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
+	var result int = 1
+	for i := 1; i <= nb; i++ {
+		oldResult := result
+		result *= i
+		if result < oldResult {
+			return 0
+		}
+	}
+
+	return result
+}

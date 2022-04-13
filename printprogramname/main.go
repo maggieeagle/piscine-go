613e6bb5868af909cdbfa3db2e8a2fbb21606ab5
maggieeagle <oryol.margo@gmail.com>
2022-04-13 12:36:33 +0300

    Print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,19 +6,15 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintStr(s string) {
-	str := []rune(s)
+func main() {
+	str := []rune(os.Args[0])
 	var index int = 0
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < len(str); i++ {
 		if str[i] == '/' {
 			index = i
 		}
 	}
-	for i := index + 1; i < len(s); i++ {
+	for i := index + 1; i < len(str); i++ {
 		z01.PrintRune(str[i])
 	}
-}
-
-func main() {
-	PrintStr(os.Args[0])
 }

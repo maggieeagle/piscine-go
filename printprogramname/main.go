c63706a2fe8e6def26baaff04f490089106a7850
maggieeagle <oryol.margo@gmail.com>
2022-04-13 11:04:14 +0300

    Print the name of the file

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -8,7 +8,13 @@ import (
 
 func PrintStr(s string) {
 	str := []rune(s)
+	var index int = 0
 	for i := 0; i < len(s); i++ {
+		if str[i] == '/' {
+			index = i
+		}
+	}
+	for i := index + 1; i < len(s); i++ {
 		z01.PrintRune(str[i])
 	}
 }

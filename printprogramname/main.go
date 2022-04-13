b6fd90ab15aa56742ae58a81bd954cb9b863c424
maggieeagle <oryol.margo@gmail.com>
2022-04-13 09:31:14 +0300

    Print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"os"
+
+	"github.com/01-edu/z01"
 )
+
+func PrintStr(s string) {
+	str := []rune(s)
+	for i := 0; i < len(s); i++ {
+		z01.PrintRune(str[i])
+	}
+}
 
 func main() {
 	println(os.Args[0])

d27fc635a499065e0f2c58c9a0e9571a1225ecba
maggieeagle <oryol.margo@gmail.com>
2022-04-14 17:58:07 +0300

    Print ints from min to max

--- appendrange.go
+++ appendrange.go
@@ -1,12 +1,27 @@
 package piscine
 
-func AppendRange(min, max int) []int {
-	arr := make([]int, 0)
-	if min > max {
-		return arr
+func CreateArrayOfSize(size int) []int {
+	var answer []int
+	for i := 0; i < size; i++ {
+		answer = append(answer, i+1)
 	}
-	for i := min; i < max; i++ {
-		arr = append(arr, i)
+	return answer
+}
+
+func AppendRange(min, max int) []int {
+
+	var size int = 0
+
+	if min < max {
+		size = max - min
+	}
+
+	arr := CreateArrayOfSize(size)
+
+	if size > 0 {
+		for i := min; i < max; i++ {
+			arr = append(arr, i)
+		}
 	}
 	return arr
 }

6178e9d60992d9f4bf9701786e7a4daeadeff648
maggieeagle <oryol.margo@gmail.com>
2022-04-15 15:52:19 +0300

    Print all numbers from min to max

--- makerange.go
+++ makerange.go
@@ -1,17 +1,17 @@
 package piscine
 
 func MakeRange(min, max int) []int {
+	var arr []int
 	if min < max {
 
-		arr := make([]int, max-min)
+		arr = make([]int, max-min)
 
 		count := min
-		for i := 0; i < len(arr); i++ {
+		for i := 0; i <= len(arr); i++ {
 			arr[i] = count
 			count++
 		}
-		return arr
 	}
-	arr := make([]int, 0)
+
 	return arr
 }

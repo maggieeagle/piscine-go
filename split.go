a065cb99a8d298e7d86c100ab186dad622b97084
maggieeagle <oryol.margo@gmail.com>
2022-04-17 07:23:05 +0300

    Split with a string

---
+++ split.go
@@ -0,0 +1,32 @@
+package piscine
+
+func getSubstring(s string, f, l int) string {
+	res := ""
+	str := []rune(s)
+	for i := f; i < l; i++ {
+		res += string(str[i])
+	}
+	return res
+}
+
+func Split(s, sep string) []string {
+	var res []string
+
+	str := []rune(s)
+
+	subs := ""
+	for i := 0; i < len(str)-len(sep)+1; i++ {
+		if getSubstring(s, i, i+len(sep)) != sep {
+			subs += string(str[i])
+		} else {
+			if len(subs) != 0 {
+				res = append(res, subs)
+				i += len(sep) - 1
+			}
+			subs = ""
+		}
+	}
+	subs += string(str[len(str)-1])
+	res = append(res, subs)
+	return res
+}

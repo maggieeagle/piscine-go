f29ce50103b6e0f2ae9fffbd69d2cf209b8c9918
maggieeagle <oryol.margo@gmail.com>
2022-04-19 15:16:00 +0300

    Read from file

---
+++ displayfile/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) == 1 {
+		fmt.Println("File name missing")
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	} else {
+
+		file, err := os.Open("quest8.txt")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		data := make([]byte, 64)
+
+		for {
+			n, err := file.Read(data)
+			if err == io.EOF { // если конец файла
+				break // выходим из цикла
+			}
+			fmt.Print(string(data[:n]))
+		}
+	}
+}

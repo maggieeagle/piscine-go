cfe192ab4733870eb9eaaca5535d41013283b42a
maggieeagle <oryol.margo@gmail.com>
2022-04-19 15:31:39 +0300

    Read from file

--- displayfile/main.go
+++ displayfile/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -13,21 +13,7 @@ func main() {
 		fmt.Println("Too many arguments")
 	} else {
 
-		file, err := os.Open("quest8.txt")
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer file.Close()
-
-		data := make([]byte, 64)
-
-		for {
-			n, err := file.Read(data)
-			if err == io.EOF { // если конец файла
-				break // выходим из цикла
-			}
-			fmt.Print(string(data[:n]))
-		}
+		dat, _ := ioutil.ReadFile("quest8.txt")
+		fmt.Print(string(dat))
 	}
 }

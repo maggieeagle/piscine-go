9190940739a6818043b7421324d5fb628b19aa3c
maggieeagle <oryol.margo@gmail.com>
2022-04-20 10:29:06 +0300

    Simplyfied cat command

---
+++ cat/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	switch len(os.Args) {
+	case 1:
+		fmt.Println("Hello\nHello\n^C")
+	case 2:
+		dat, err := ioutil.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Println("ERROR: open " + os.Args[1] + ": no such file or directory")
+		}
+		fmt.Print(string(dat))
+	case 3:
+		dat, err := ioutil.ReadFile(os.Args[1])
+		dat2, err2 := ioutil.ReadFile(os.Args[2])
+		if err != nil {
+			fmt.Println("ERROR: open " + os.Args[1] + ": no such file or directory")
+		}
+		fmt.Print(string(dat))
+		if err2 != nil {
+			fmt.Println("ERROR: open " + os.Args[2] + ": no such file or directory")
+		}
+		fmt.Print(string(dat2))
+	}
+}

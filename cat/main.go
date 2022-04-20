afb33c319084b5119dd148bebd60b4905216fcc4
maggieeagle <oryol.margo@gmail.com>
2022-04-20 13:44:22 +0300

    With another read

--- cat/main.go
+++ cat/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io"
-	"io/ioutil"
 	"os"
 
 	"github.com/01-edu/z01"
@@ -14,34 +13,26 @@ func printStr(s string) {
 	}
 }
 
-func main() {
-	switch len(os.Args) {
-	case 1:
-		io.Copy(os.Stdout, os.Stdin)
+func readFromFile(f string) {
+	file, err := os.Open(f)
+	if err != nil {
+		printStr("ERROR: " + err.Error())
+		z01.PrintRune('\n')
+		os.Exit(1)
+	}
+	dat, _ := file.Stat()
+	content := make([]byte, dat.Size())
+	file.Read(content)
+	printStr(string(content))
+	file.Close()
+}
 
-	case 2:
-		dat, err := ioutil.ReadFile(os.Args[1])
-		if err != nil {
-			printStr("ERROR: " + err.Error())
-			z01.PrintRune('\n')
-			os.Exit(1)
-		} else {
-			printStr(string(dat))
-		}
-	case 3:
-		dat, err := ioutil.ReadFile(os.Args[1])
-		dat2, err2 := ioutil.ReadFile(os.Args[2])
-		if err != nil {
-			printStr("ERROR: " + err.Error())
-			z01.PrintRune('\n')
-		} else {
-			printStr(string(dat))
-		}
-		if err2 != nil {
-			printStr("ERROR: " + err.Error())
-			z01.PrintRune('\n')
-		} else {
-			printStr(string(dat2))
+func main() {
+	if len(os.Args) == 1 {
+		io.Copy(os.Stdout, os.Stdin)
+	} else {
+		for i := 1; i < len(os.Args); i++ {
+			readFromFile(os.Args[i])
 		}
 	}
 }

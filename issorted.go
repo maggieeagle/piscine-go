5a131bf5d8275b2a8e99e23e957c47172e15c09b
maggieeagle <oryol.margo@gmail.com>
2022-04-20 18:25:56 +0300

    Is array sorted

--- issorted.go
+++ issorted.go
@@ -2,7 +2,8 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) > 0 {
+		println(a[i])
+		if f(a[i], a[i+1]) < 0 {
 			return false
 		}
 	}

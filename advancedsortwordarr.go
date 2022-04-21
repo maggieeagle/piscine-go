369e44f9c2cbfee7ca130f47a403ce20192456fe
maggieeagle <oryol.margo@gmail.com>
2022-04-21 20:05:17 +0300

    With a free function

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -5,7 +5,7 @@ func AdvancedSortWordArr(a []string, f func(a, b string) int) {
 
 	for i := 0; i < len(params)-1; i++ {
 		for j := i + 1; j < len(params); j++ {
-			if Compare(params[i], params[j]) == 1 {
+			if f(params[i], params[j]) == 1 {
 				tmp := params[i]
 				params[i] = params[j]
 				params[j] = tmp

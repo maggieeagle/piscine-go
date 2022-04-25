908e3b2c0ccc33c6a6f6534bed780d1e1c3cf497
maggieeagle <oryol.margo@gmail.com>
2022-04-25 07:50:23 +0300

    Sort linked list

---
+++ listsort.go
@@ -0,0 +1,31 @@
+package piscine
+
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+func ListSort(l *NodeI) *NodeI {
+	node := l
+
+	if node == nil {
+		return nil
+	}
+	for node != nil {
+		next := node.Next
+		for next != nil {
+			if node.Data > next.Data {
+				swap(node, next)
+			}
+			next = next.Next
+		}
+		node = node.Next
+	}
+	return node
+}
+
+func swap(a, b *NodeI) {
+	tmp := a.Data
+	a.Data = b.Data
+	b.Data = tmp
+}
